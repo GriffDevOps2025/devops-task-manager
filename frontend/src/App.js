fb2b1e3a6fe7f8532f0accf6f137eafb062c5e50
GriffDevOps2025 <griffindaniel48@gmail.com>
2025-08-09 00:42:54 -0400

    Fix frontend build: lowercase public/, move files to src/, update nginx.conf for local proxy

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DevOps Task Manager</h1>
+      <p>Frontend is running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
